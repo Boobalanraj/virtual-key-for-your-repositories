de273626cfc6a74d3e1b89606ea929f0b6290a59
Boobalanraj <boobalan.raj30072001@gmail.com>
2022-07-30 20:22:30 +0530

    Virtual key for your repositories

--- virtual key for your repositories/src/com/virtualkey/practice/Directory.java
+++
@@ -1,9 +0,0 @@
-package com.virtualkey.practice;
-
-import java.io.File;
-
-public class Directory {
-
-public static File location = new File("C:\\Users\\BoobalanRaj\\Desktop\\project\\main\\");
-
-}
